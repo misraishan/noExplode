5bae05a5235ede85a4717a767003c1f2421a2bd1
HayHay <dev@hayhay.dev>
2025-07-08 23:19:36 -0400

    Update for 1.21 and change stuff

--- src/main/java/hayhay/cc/noExplode/noExplodeListener.java
+++ src/main/java/hayhay/cc/noExplode/noExplodeListener.java
@@ -1,15 +1,34 @@
 package hayhay.cc.noExplode;
 
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 public class noExplodeListener implements Listener {
     @EventHandler
     public void onCreeperExplode(EntityExplodeEvent event) {
         switch (event.getEntityType()) {
-            case CREEPER: event.setCancelled(true);
-            case FIREBALL: event.setCancelled(true);
+            case CREEPER:
+                event.blockList().clear();
+                break;
+            case FIREBALL:
+                event.blockList().clear();
+                break;
+            default:
+                break;
+        }
+    }
+
+    @EventHandler
+    public void onItemDamage(EntityDamageEvent event) {
+        if (event.getEntity() instanceof Item) {
+            // Check for any explosion-related damage
+            if (event.getCause() == EntityDamageEvent.DamageCause.ENTITY_EXPLOSION ||
+                    event.getCause() == EntityDamageEvent.DamageCause.BLOCK_EXPLOSION) {
+                event.setCancelled(true);
+            }
         }
     }
 }
